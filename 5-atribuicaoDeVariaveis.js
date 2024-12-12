34e9a383ab6b5f535b4e3a88cab4885011b25879
Bonilhajvv <joaovictorbonilhaventurinib@gmail.com>
2024-12-12 17:15:02 -0300

    feat: demonstra concatenação de strings com operadores e template literals

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+idade=29;
+const nome = "Joao";
+const sobreNome = "Bonilha";
+console.log(nome+ " " +sobreNome);
+
+console.log(`meu nome é ${nome} ${sobreNome}`);
+
+let nome1 = "Victor";
+let sobreNome1 = "Venturini";
+
+nome1 = nome1 + sobreNome1;
+
+
+console.log(nome1);
+
