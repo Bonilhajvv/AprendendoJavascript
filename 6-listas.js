134287970e2b255b3e59fef5ee30b1720c1c4d21
Bonilhajvv <joaovictorbonilhaventurinib@gmail.com>
2024-12-12 17:55:32 -0300

    feat: demonstra manipulação de arrays com push e listagem de destinos

---
+++ 6-listas.js
@@ -0,0 +1,15 @@
+console.log("trabalhando com Listas");
+
+const salvador = `Salvador`;
+const saoPaulo =  `Sao Paulo`;
+const rioDeJaneiro = `Rio de Janeiro`;
+
+console.log(salvador, saoPaulo, rioDeJaneiro);
+
+const listaDeDestinos = new Array(
+    `Salvador`,   
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+listaDeDestinos.push(`Curitiba`);
+console.log(listaDeDestinos);
